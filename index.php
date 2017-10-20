e307da0ac1bb879e39ab9e0caf671f5840c23fc6
LAPTOP-SANDER\sande <sander.nieuwenhuisen@outlook.com>
2017-10-20 12:16:01 +0200

    main database connection

--- index.php
+++ index.php
@@ -6,14 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Databases</title>
 
-    <!-- <link href="stylesheet.css" rel="stylesheet" type="text/css"> -->
+    <link href="stylesheet.css" rel="stylesheet" type="text/css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
   </head>
   <body>
-    <br />
-    <br />
-    <div class="sidebar col-md-4">
+    <?php include("connect.php"); ?>
+    <div id="sidebar" class="col-md-3">
       <div class="panel panel-default">
         <div class="panel-heading">Databases</div>
         <div class="panel-body">Panel Content
