9996e3c4b6977edd9d7c0eeade55a11b2a9ca8e1
LAPTOP-SANDER\sande <sander.nieuwenhuisen@outlook.com>
2017-10-20 11:52:06 +0200

    added index.php, bootstrap and an empty css

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Databases</title>
+
+    <!-- <link href="stylesheet.css" rel="stylesheet" type="text/css"> -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <br />
+    <br />
+    <div class="sidebar col-md-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">Databases</div>
+        <div class="panel-body">Panel Content
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
